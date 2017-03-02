fcc0b339eb68996fce2b81276314f4f524a691f4
madhu-patidar <madhusudan.patidar@neosofttech.com>
2017-03-02 20:25:23 +0530

    add demo app

---
+++ post/app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+ 
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to root_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
+  def post_params
+    params.require(:post).permit(:user_name, :title, :body)
+  end
+end
--- test/controllers/articals_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ArticalsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
